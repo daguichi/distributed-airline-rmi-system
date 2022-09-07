dd15f5c5ee0dd61aebf416a3773b474e8421c344
Santiago Sandrini <ssandrini@itba.edu.ar>
2022-09-07 16:01:28 -0300

    Server impl

--- tpe1-g9/server/src/main/java/ar/edu/itba/pod/server/Server.java
+++ tpe1-g9/server/src/main/java/ar/edu/itba/pod/server/Server.java
@@ -3,10 +3,32 @@ package ar.edu.itba.pod.server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 public class Server {
     private static Logger logger = LoggerFactory.getLogger(Server.class);
 
-    public static void main(String[] args) {
-        logger.info("tpe1-g9 Server Starting ...");
+    public static void main(String[] args) throws RemoteException {
+        final Servant servant = new Servant();
+        final Remote remote = UnicastRemoteObject.exportObject(servant, 0);
+
+        int port = 1099;
+        if (args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (Exception e) {
+                logger.error("Invalid port argument");
+            }
+        }
+
+        final Registry registry = LocateRegistry.getRegistry("localhost", port);
+        registry.rebind("flight_administration", remote);
+        registry.rebind("flight_notification", remote);
+        registry.rebind("seat_administration", remote);
+        registry.rebind("seat_map", remote);
     }
 }
